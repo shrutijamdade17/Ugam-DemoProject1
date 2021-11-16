0ac292f9dd569ce312965508a0bb0c11a8fafd7c
Shruti Jamdade <shruti.jamdade@ugamsoluions.com>
2021-11-16 16:11:31 +0530

    Sling Servlet Using Path Added

---
+++ ugamdemo/core/src/main/java/com/ugamdemo/core/models/impl/servletDemo.java
@@ -0,0 +1,27 @@
+package com.ugamdemo.core.models.impl;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import java.io.IOException;
+
+@Component(service = Servlet.class)
+@SlingServletPaths(
+        value = {"/bin/demoServlet"}
+)
+public class servletDemo extends SlingAllMethodsServlet {
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws IOException {
+        String val1 = request.getParameter("val1");
+        String val2 = request.getParameter("val2");
+        int value1 = Integer.parseInt(val1);
+        int value2 = Integer.parseInt(val2);
+        int sum = value1+value2;
+        response.getWriter().println(sum);
+    }
+}
