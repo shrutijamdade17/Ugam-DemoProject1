4cb9306f8224456b1de94057b76276c114601209
Shruti Jamdade <shruti.jamdade@ugamsoluions.com>
2021-11-25 19:38:28 +0530

    added QueryBuilder For Usernames with Service User

---
+++ ugamdemo/core/src/main/java/com/ugamdemo/core/models/UserNameModel.java
@@ -0,0 +1,10 @@
+package com.ugamdemo.core.models;
+
+import com.day.cq.wcm.api.Page;
+
+import java.util.Iterator;
+
+public interface UserNameModel {
+    public String getUsernames();
+    //public Iterator<Page> getPages();
+}
---
+++ ugamdemo/core/src/main/java/com/ugamdemo/core/services/UserNameService.java
@@ -0,0 +1,7 @@
+package com.ugamdemo.core.services;
+
+import java.util.List;
+
+public interface UserNameService {
+    public String getUsernames();
+}
